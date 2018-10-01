d0531eecbc74802339524562cc746272f55dafbb
Aristoteles Coutinho <aristotelesbr@coutinho.com>
2018-10-01 14:25:46 -0300

    added mongoid gem and route to create atweet

--- App.rb
+++ App.rb
@@ -1,6 +1,27 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/contrib'
 require 'sinatra/reloader'
 require 'sinatra/namespace'
+require 'mongoid'
 
-get ('/api/v1/tweets') { 'Usando askdjhasdk' }
+Mongoid.load! 'mongoid.config'
+
+class Atweet
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :author, type: String, default: 'ANON'
+  field :content, type: String
+  field :image, type: String
+  field :tags, type: Array, default: []
+  field :likes, type: Integer, default: 0
+end
+
+get ('/api/v1/atweets') { Atweet.all.to_json }
+
+post '/api/v1/atweets' do
+  new_atweet = Atweet.new(JSON.parse(request.body.read)).save
+  new_atweet.to_json
+end
--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '~> 2.0', '>= 2.0.4'
 gem 'sinatra-contrib', '~> 2.0', '>= 2.0.4'
+# Mongoid is an ODM (Object Document Mapper) Framework for MongoDB, written in Ruby.
+gem 'mongoid', '~> 7.0', '>= 7.0.2'
